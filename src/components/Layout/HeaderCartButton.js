0128b5662aec6a84b7d0c7e2a3f0ba64f557fa30
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-02-12 20:01:10 +0530

    added bump animation for cart button on adding of an item

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,41 @@
 import buttonStyles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function HeaderCartButton(props) {
+  const [animateButton, setAnimateButton] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfItems = cartCtx.items.reduce((currentVal, item) => {
-    return currentVal + item.amount
+    return currentVal + item.amount;
   }, 0);
+
+  const btnClasses = `${buttonStyles.button} ${
+    animateButton ? buttonStyles.bump : ""
+  }`;
+
+  useEffect(() => {
+    let animationTimer;
+
+    if (cartCtx.items.length) {
+      setAnimateButton(true);
+      animationTimer = setTimeout(() => {
+        setAnimateButton(false);
+      }, 300);
+    }
+
+    return () => {
+      clearTimeout(animationTimer)
+    }
+  }, [cartCtx.items]);
+
   return (
-    <button className={buttonStyles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={buttonStyles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={buttonStyles.badge}> { numberOfItems } </span>
+      <span className={buttonStyles.badge}> {numberOfItems} </span>
     </button>
   );
 }
