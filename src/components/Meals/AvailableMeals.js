b794ee7d31e0c5c00df576efc8a63a60ec40bb15
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-02-24 13:46:24 +0530

    now bringing the meals dynamically through firebase API calls

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,52 +1,74 @@
 import availableMealsStyles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import appConstants from "../../store/constants";
+import { useCallback, useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 20,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 20,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 export default function AvailableMeals() {
-  const mealItems = DUMMY_MEALS.map((item) => (
-    <MealItem
-      key={item.id}
-      description={item.description}
-      price={item.price}
-      id={item.id}
-      name={item.name}
-    >
-      {item.name}
-    </MealItem>
-  ));
+  const [mealItems, setMealItems] = useState();
+
+  const fetchAllMeals = useCallback(async () => {
+    const mealsUrl = `${appConstants.BASE_URL}${appConstants.MEALS_EXTENSION}`;
+    const response = await fetch(mealsUrl);
+    const mealsObj = await response.json();
+    const meals = [];
+    Object.keys(mealsObj).forEach(key => {
+      const meal = {...mealsObj[key], id: key}
+      meals.push(meal);
+    });
+    const mealsJSX = meals.map((item) => (
+      <MealItem
+        key={item.id}
+        description={item.description}
+        price={item.price}
+        id={item.id}
+        name={item.name}
+      >
+        {item.name}
+      </MealItem>
+    ));
+    setMealItems(mealsJSX);
+  }, []);
+
+  useEffect(() => {
+    fetchAllMeals();
+  }, [fetchAllMeals])
 
   return (
     <section className={availableMealsStyles.meals}>
-      <Card>
+      { mealItems && <Card>
         <ul>{mealItems}</ul>
       </Card>
+      }
+      { !mealItems && <p>Loading the meals...</p> }
+      
     </section>
   );
 }
---
+++ src/store/constants.js
@@ -0,0 +1,7 @@
+const appConstants = {
+    BASE_URL: 'https://react-http-1e532-default-rtdb.firebaseio.com/',
+    MEALS_EXTENSION: 'meals.json'
+}
+
+Object.freeze(appConstants);
+export default appConstants;
